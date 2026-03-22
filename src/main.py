59889ccdc4a273998dcab8531539d7185a605770
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-03-22 19:08:06 +0500

    feat: add ground entity

--- src/main.py
+++ src/main.py
@@ -3,6 +3,13 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 
+ground = Entity(
+    model="plane",
+    scale=(200, 20, 200),
+    color=color.green,
+    collider="mesh",
+)
+
 player = FirstPersonController()
 
 app.run()
